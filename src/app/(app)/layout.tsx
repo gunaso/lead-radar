7afbd9aa9234b21791a692d4337a84323e09ab01
gunaso <ferreira.andre98@gmail.com>
2025-10-14 15:45:01 +0900

    Formatting

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,9 +1,9 @@
 import type { ReactElement, ReactNode } from "react"
 
 import { SidebarProvider, SidebarInset } from "@/components/ui/sidebar"
+import { HeaderProvider } from "@/components/header/header-context"
 import Sidebar from "@/components/sidebar/sidebar"
 import Header from "@/components/header/header"
-import { HeaderProvider } from "@/components/header/header-context"
 
 export default function AppShellLayout({
   children,
